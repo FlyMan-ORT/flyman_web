375de9e681ee9e39d5c48b2b6b9bcf53dbe687a5
dtlucho <luciano.ditomaso@gmail.com>
2022-05-08 19:07:30 -0300

    adding axios to fetch cars

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,64 +1,56 @@
-import {Table  } from 'reactstrap';
-import Row from './Row'
+import { Table } from 'reactstrap';
+import Row from './Row';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function Home(){       
-     
-    let miArray = [
-    {
-        patente: "123",
-        marca: "ford",
-        modelo: "focus",
-        ubicacion: "4F"
-    },
-    {
-        patente: "456",
-        marca: "ford",
-        modelo: "frio",
-        ubicacion: "5F"
-    },
-    {
-        patente: "789",
-        marca: "ford",
-        modelo: "astra",
-        ubicacion: "8C"
-    },
-    {
-        patente: "321",
-        marca: "ford",
-        modelo: "faca",
-        ubicacion: "1A"
-    },
-]
+function Home() {
+    const [cars, setCars] = useState([]);
 
-    return(
-        <div>          
+    useEffect(() => {
+        console.log('use effect');
+        async function fetchData() {
+            const response = await axios.get('http://192.168.68.106:3000/cars/');
+            setCars(response.data);
+        }
+        fetchData();
+    }, [])
+
+
+    return (
+        <div>
             <Table hover>
-            <thead>
-                <tr>
-                <th>
-                    Patente
-                </th>
-                <th>
-                    Marca
-                </th>
-                <th>
-                    Modelo
-                </th>
-                <th>
-                    Ubicación
-                </th>
-                </tr>
-            </thead>
+                <thead>
+                    <tr>
+                        <th>
+                            Patente
+                        </th>
+                        <th>
+                            Modelo
+                        </th>
+                        <th>
+                            Nivel de combustible
+                        </th>
+                        <th>
+                            Tipo de combustible
+                        </th>
+                        <th>
+                            Parking
+                        </th>
+                        <th>
+                            Ubicación
+                        </th>
+                    </tr>
+                </thead>
                 <tbody>
                     {
-                        miArray.map((item)=> {
-                            return <Row auto={item}/>
+                        cars.map((car) => {
+                            return <Row auto={car} key={car.id} />
                         })
-                    }                        
+                    }
                 </tbody>
             </Table>
         </div>
-        
+
     );
 }
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,22 +1,28 @@
 
 
-function Row(props){
-return(
-    <tr>
-    <th scope="row">
-        {props.auto.patente}
-    </th>
-    <td>
-        {props.auto.marca}
-    </td>
-    <td>
-        {props.auto.modelo}
-    </td>
-    <td>
-        {props.auto.ubicacion}
-    </td>                    
-  </tr>
-);
+function Row(props) {
+    return (
+        <tr>
+            <th scope="row">
+                {props.auto.plate}
+            </th>
+            <td>
+                {props.auto.description}
+            </td>
+            <td>
+                {props.auto.fuelLevel}
+            </td>
+            <td>
+                {props.auto.fuelType}
+            </td>
+            <td>
+                {props.auto.parkingName}
+            </td>
+            <td>
+                {props.auto.idParkingSlot}
+            </td>
+        </tr>
+    );
 }
 
 export default Row;
