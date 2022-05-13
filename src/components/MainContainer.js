06e0bf5fca3fad48d8b0cd28545f029533cb5002
Leo-Fer <leonardo.fernandez.g@gmail.com>
2022-05-13 19:21:24 -0300

    Added MainContainer component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+function MainContainer() {
+  return (    
+      <div style={{ display: 'flex', flexDirection: 'row' }}>      
+        <NavBar/> 
+      </div>    
+  );
+}
+
+export default MainContainer;
