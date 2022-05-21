b44a1912c796d004d7b09b994991efb738c895ff
Leo-Fer <leonardo.fernandez.g@gmail.com>
2022-05-21 00:21:38 -0300

    Login form and styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card'
 
 const loginButtonStyle = {
     marginLeft: 'auto',
     paddingRight: 10
 }
 
+const containerDivStyle = { 
+    display: 'flex', 
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    justifyContent: 'center' 
+}
+
 function Login() {
     return (
-        <div >
-            <h1>Bienvenidos a MyKeego</h1>
-            <div style={loginButtonStyle}>
-                <Button variant="contained">Iniciar Sesión</Button>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: 200 }}>
+
+            <div style={containerDivStyle}>
+                <Card>
+                    <Card.Header as="h5">Panel de administrador Keego</Card.Header>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Usuario</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    onChange={() => { }}
+                                    autoFocus
+                                />
+                                <Form.Label>Contraseña</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    onChange={() => { }}
+                                />
+                            </Form.Group>
+                        </Form>
+                        <Button variant="contained" style={{width:'100%'}}>Iniciar Sesión</Button>
+                    </Card.Body>
+                </Card>
             </div>
 
         </div>
