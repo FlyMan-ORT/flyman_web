aa97127f296d5960ae7a30de8065a35b5eabd732
Fernando Pallas <ferpallas@gmail.com>
2022-06-04 21:25:06 -0300

    feature/ structur user modal assignation

--- src/utils/connections.js
+++ src/utils/connections.js
@@ -2,6 +2,7 @@ const connections = {
   genteNormal: 'http://192.168.68.110:3000',
   fermin: 'http://192.168.0.8:3000',
   eze: 'http://192.168.0.4:3000',
+  fer: 'http://192.168.68.54:3000'
 }
 
 
@@ -9,5 +10,5 @@ const getConnection = (nombre) => {
   return connections[`${nombre}`];
 }
 
-export const BASE_URL = getConnection('fermin');
+export const BASE_URL = getConnection('fer');
 
