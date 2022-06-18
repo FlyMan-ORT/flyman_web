9e9394c50e2b6f76ccd243b6e512132ace853570
Leo-Fer <leonardo.fernandez.g@gmail.com>
2022-06-18 14:54:40 -0300

    update style for nav menu

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -4,12 +4,18 @@ import Home from '../screens/Home';
 import Users from '../screens/Users';
 import Historic from '../screens/Historic';
 import Button from '@mui/material/Button';
-import CarCrashIcon from '@mui/icons-material/CarCrash';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-
 import { Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
 
 const linkStyle = {
     textDecoration: "none",
@@ -27,7 +33,7 @@ const navBarDivStyle = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    padding: 15,    
+    padding: 15,
 
 };
 
@@ -39,33 +45,36 @@ function NavBar() {
 
     return (
         <Router>
-            <div style={{marginLeft: 10}}>
-            <Button onClick={() => setDrawerShow(!drawerShow)}><MenuIcon></MenuIcon></Button>
+            <div style={{ marginLeft: 10 }}>
+                <Button onClick={() => setDrawerShow(!drawerShow)}><MenuIcon></MenuIcon></Button>
             </div>
-            
+
             <Drawer open={drawerShow} onClose={() => handleClick()}>
                 <div style={{ backgroundColor: '#e5e5e5', width: '100%', padding: 10 }}>
                     <img style={{ width: 100 }} src="https://www.mykeego.com/img/KEEGOblanco.png"></img>
                 </div>
-                
-                <div style={navBarDivStyle}>
-                    <Link to="/" style={linkStyle}>
-                        <Button variant="text" style={buttonStyle} onClick={() => setDrawerShow(false)} >
-                        <CarCrashIcon alignSelf="left"/> Autos
-                        </Button>
-                    </Link>
-                    <Link to="/Users" style={linkStyle}>
-                        <Button variant="text" style={buttonStyle} onClick={() => setDrawerShow(false)}>
-                          <PeopleAltIcon/>  Usuarios
-                        </Button>
-                    </Link>
-                    <Link to="/Historic" style={linkStyle}>
-                        <Button variant="text" style={buttonStyle} onClick={() => setDrawerShow(false)}>
-                        <ManageSearchIcon/>    Historial 
-                        </Button>
-                    </Link>
-                </div>
 
+                <Box sx={{ width: 180, padding: 0 }}>
+                    <List>
+                        {[
+                            { key:'/',page: '/', text: 'Autos' },
+                            { key:'Users',page: '/Users', text: 'Usuarios' },
+                            { key:'Historic',page: '/Historic', text: 'Historial' },
+                        ].map((menuItem) => (
+                            <ListItem key={menuItem.key} style={{ padding: 0 }}>
+                                <Link to={menuItem.page} style={{ textDecoration: 'none', color: '#000' }}>
+                                    <ListItemButton onClick={() => setDrawerShow(false)}>
+                                        <ListItemIcon>
+                                            <DirectionsCarIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary={menuItem.text} style={{ width: 120, padding: 8 }} />
+                                    </ListItemButton>
+                                </Link>
+                            </ListItem>
+                        ))}
+
+                    </List>
+                </Box>
             </Drawer>
 
             <Routes>
@@ -74,7 +83,7 @@ function NavBar() {
                 <Route path="/Historic" element={<Historic />} />
             </Routes>
 
-        </Router>
+        </Router >
     );
 };
 
